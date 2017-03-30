641ad84bba0330492404c43559fac6c84bfa57b2
Melwin <melwinkieffer@gmail.com>
2017-03-30 11:02:14 +0200

    ops: Save dev config file

--- start-dev.sh
+++ start-dev.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-name='gitlab-webhook-translator'
+name="gitlab-webhook-translator"
 
 docker build -f docker/Dockerfile -t ${name} .
 docker stop ${name} && docker rm ${name}
-docker run -d --name ${name} -e NODE_ENV=dev -e VIRTUAL_HOST="~^wht\..*\.xip\.io" -v $(pwd):/app ${name} nodemon start
+
+home=~
+localpath="${home}/docker/${name}/"
+filename="translations.json"
+
+if [ ! -f ${localpath}${filename} ]; then
+    mkdir -p ${localpath}
+    touch ${localpath}${filename} && cp ./docker/translations.json ${localpath}${filename}
+    echo "Config file created at ${localpath}${filename}"
+fi
+
+docker run -d --name ${name} -e NODE_ENV=dev \
+-v ~/docker/${name}/translations.json:/translations.json \
+-e VIRTUAL_HOST="~^wht\..*\.xip\.io" \
+-v $(pwd):/app ${name} \
+nodemon start
+
 docker logs -f ${name}
