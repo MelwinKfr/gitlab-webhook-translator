c15b5a6bdf2020245015541db2ccb6899d8fd83f
Nahratow Philipp 8IP1Q <philipp.nahratow@rohde-schwarz.com>
2018-09-26 15:37:20 +0200

    Add optional base image build an port expose to start-dev

--- start-dev.sh
+++ start-dev.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+EXPOSE_PORT=$1
+if [ ! -z "$EXPOSE_PORT" ]; then
+  EXPOSE_CMD="-p $EXPOSE_PORT:80"
+fi
 
 name="gitlab-webhook-translator"
 
-# Intsall dev dependencies
+# Install dev dependencies
 docker run --rm -it -v $(pwd):/app -w /app node npm install
 
 # Build containers
+if [[ "$(docker images -q wht 2> /dev/null)" == "" ]]; then
+  docker build -t wht .
+fi
 docker build -f docker/Dockerfile-dev -t wht-dev .
 
 # Stop and remove old instance
 docker stop ${name} && docker rm ${name}
 
 # Create configuration if needed
-home=~
-localpath="${home}/docker/${name}/"
+localpath="${HOME}/docker/${name}/"
 filename="translations.json"
 if [ ! -f ${localpath}${filename} ]; then
     mkdir -p ${localpath}
@@ -26,6 +32,7 @@ docker run -d --name ${name} -e NODE_ENV=dev \
 -v ~/docker/${name}/translations.json:/translations.json \
 -e VIRTUAL_HOST="~^wht\..*\.xip\.io" \
 -v $(pwd):/app \
+$EXPOSE_CMD \
 wht-dev \
 nodemon start
 
